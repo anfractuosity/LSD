f0d8bcd7c4b01f9c241e7bed70249203ce07d940
Chris <chris@anfractuosity.com>
2014-09-20 03:19:26 +0200

    readme

--- Line.java
+++ Line.java
@@ -41,22 +41,14 @@ public class Line {
 	}
 
 	Line(double x1, double y1, double x2, double y2) {
-
-		//double d1 = distance(0, 0, x1, y1);
-		//double d2 = distance(0, 0, x2, y2);
-
 		if (y1 < y2) {
-
 			this.x1 = x1;
 			this.y1 = y1;
 			this.x2 = x2;
 			this.y2 = y2;
 
-
 			start = new Point(this.x1, this.y1, 0);
 			end = new Point(this.x2, this.y2, 0);
-			// xDiff = x2 - x1;
-			// yDiff = y2 - y1;
 		} else {
 
 			this.x1 = x2;
@@ -67,40 +59,16 @@ public class Line {
 
 			end = new Point(this.x1, this.y1, 0);
 			start = new Point(this.x2, this.y2, 0);
-
-			// xDiff = x1 - x2;
-			// yDiff = y1 - y2;
 		}
-		
-		
-		/*
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
-		*/
 		
 		xDiff = this.x2 - this.x1;
 		yDiff = this.y2 - this.y1;
-
-		//start = new Point(this.x1, this.y1, 0);
-		//end = new Point(this.x2, this.y2, 0);
 
 		this.height = yDiff;
 		this.width = xDiff;
 
 		this.length = distance(x1, y1, x2, y2);
 
-		// angle = Math.atan2(yDiff, xDiff) * 180/Math.PI;
-
-		//double n1 = Math.sqrt(this.x1 * this.x1 + this.y1 * this.y1), n2 = Math.sqrt(this.x2 * this.x2 + this.y2
-		//		* this.y2);
-		//angle = Math.acos((this.x1 * this.x2 + this.y1 * this.y2) / (n1 * n2)) * 180 / Math.PI;
-
 		angle = Math.toDegrees( Math.atan2(yDiff, xDiff));
-		
-		
-		//System.out.println(angle);
-
 	}
 }
--- README.md
+++ README.md
@@ -4,3 +4,10 @@ LSD
 
 A Java port of the excellent Line Segment Detector algorithm from:
 http://www.ipol.im/pub/art/2012/gjmr-lsd/
+
+
+Compilation
+======================
+
+Simply run : javac *.java
+Then java GUI
